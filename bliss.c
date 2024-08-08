2259195910c7c4ccc70725b7efa8c7778d79cd6f
aycollapse <81759055+aycollapse@users.noreply.github.com>
2024-08-09 01:30:01 +0200

    added window size detection, cleaner code

--- bliss.c
+++ bliss.c
@@ -3,23 +3,34 @@
 #include <errno.h> //error handling
 #include <stdlib.h> //general functions
 #include <ctype.h>  //character classification
+#include <sys/ioctl.h> //get terminal window size
 #include <unistd.h> //access POSIX operating system API functions
 #include <termios.h> //configure Unix system terminal options
 
-/*** defines ***/
-#define CTRL_KEY(k) ((k) & 0x1f)
+/*** defines and signatures ***/
+#define CTRL_KEY(k) ((k) & 0x1f) //every CTRL_KEY(k) is replaced with (k & 0x1f) where k is a char
+void editorClear();
 
 /*** Data ***/
-//save original termios attributes
-struct termios og_termios; 
+struct editorConfig
+{
+    //terminal windows size
+    int screenrows;
+    int screencols;
+    //save original termios attributes
+    struct termios og_termios;
+};
+ 
+ struct editorConfig Config;
 
 /*** Terminal ***/
 //error handling
 void die(const char *s) 
 {
+    editorClear();
     //check global errno variable and describe error
     perror(s);
-    //exit process
+    //exit process failure
     exit(1);
 }
 
@@ -27,19 +38,19 @@ void die(const char *s)
 void disableRawMode()
 {
     //write original attributes
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &og_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &Config.og_termios) == -1)
     die("tcsetattr");
 }
 
 //ovverride terminal canonical mode
-void enablerRawMode() 
+void enableRawMode() 
 {
     /*save terminal attributes on struct */
 
-    if(tcgetattr(STDIN_FILENO, &og_termios)==-1) die("tcgetattr");
+    if(tcgetattr(STDIN_FILENO, &Config.og_termios)==-1) die("tcgetattr");
     //called at exit of process regardless of position in code
     atexit(disableRawMode); 
-    struct termios raw = og_termios;
+    struct termios raw = Config.og_termios;
 
     /* modify attributes */
 
@@ -67,23 +78,86 @@ void enablerRawMode()
     */
 }
 
+char editorReadKey()
+{
+    int num_read;
+    char c;
+    //reads an exact byte of data and then returns if there are no errors
+    while ((num_read = read(STDIN_FILENO, &c, 1))!=1)
+    {
+        //check if there is an error that is not EAGAIN, but something more serious
+        if(num_read == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    //retrieve terminal size if possible, otherwise return error to initEditor
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) return -1;
+    else 
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** Input ***/
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+
+    switch (c)
+    {
+        case CTRL_KEY('q'):
+            editorClear();
+            //exit process success
+            exit(0);
+            break;
+    }
+}
+
+/*** Output ***/
+void editorDrawRows()
+{
+    for(int i=0;i < Config.screenrows; i++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3); // return carrier \r is always needed in raw mode
+    }
+    write(STDOUT_FILENO, "~", 1); //last line
+}
+
+void editorClear() {
+    //clear terminal
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    //reset cursor position
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorRefreshScreen()
+{
+    editorClear();
+    editorDrawRows();
+    //reset cursor position after draw
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** Init ***/
+void initEditor()
+{
+    //set terminal size in config at startup if possible
+    if(getWindowSize(&Config.screenrows, &Config.screencols)==-1) die("getWindowsize");
+}
+
 int main()
 {
-    enablerRawMode();   
-    char c;
+    enableRawMode();
+    initEditor(); 
     while (1)
     {
-        c='\0';
-        //check if there is an error that is not EAGAIN, but something more serious
-        if(read(STDIN_FILENO, &c, 1)==-1 && errno != EAGAIN)die("read");
-        
-        // \r carrier return, puts cursor on the leftmost position on newline
-        //if char is control char print only ASCII value
-        if(iscntrl(c)) printf("%d\r\n",c); 
-        else printf("%d ('%c')\r\n", c, c); 
-        
-        if(c=='q')break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
